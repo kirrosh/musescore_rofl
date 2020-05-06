3118655ba4945e963a811aa8969fd83c92cca582
Kirill <olegartor7@gmail.com>
2020-05-07 00:11:00 +0300

    Updates

---
+++ precache-manifest.7ea45453586073bf7fade55a3492e616.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1beac91e74453daa3c250013b022cd6",
+    "url": "/musescore_rofl/index.html"
+  },
+  {
+    "revision": "78d1d069000a5ed7d256",
+    "url": "/musescore_rofl/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "e32d0ca2555caa3861df",
+    "url": "/musescore_rofl/static/css/main.b7de0216.chunk.css"
+  },
+  {
+    "revision": "78d1d069000a5ed7d256",
+    "url": "/musescore_rofl/static/js/2.ef1b5c82.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/musescore_rofl/static/js/2.ef1b5c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32d0ca2555caa3861df",
+    "url": "/musescore_rofl/static/js/main.cc499232.chunk.js"
+  },
+  {
+    "revision": "0071c6a083658d7b7225",
+    "url": "/musescore_rofl/static/js/runtime-main.c3f3fa3a.js"
+  },
+  {
+    "revision": "7f6184f65a780865386ae7d9f8f60f20",
+    "url": "/musescore_rofl/static/media/fakeSheet.7f6184f6.svg"
+  }
+]);
